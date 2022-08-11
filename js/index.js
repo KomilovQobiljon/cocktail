2ffc5071137fe7e4a1534584eab2b1e246222758
KomilovKobiljon <mr.qmi5plus@gmail.com>
2022-08-11 18:05:40 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var mainTestimonials = document.querySelector(".main__testimonials");
+
+var TestimonialsFlkty = new Flickity(mainTestimonials, {
+  // options
+  cellAlign: "left",
+  contain: true,
+});
